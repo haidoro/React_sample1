c877411bb4e8fa16403e3d582c0ca00750a7c88e
Tahara <tahara@studiom-web.net>
2018-08-07 14:18:05 +0900

    コンポーネントをAppファイルにモジュール化

--- src/App.js
+++ src/App.js
@@ -2,20 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+class Count extends React.Component{
+  constructor(props) {
+    super(props); 
+    this.state = {number: 0};
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>カウンター</h1>
+        <div className="wrap">
+        <div className="num">{this.state.number}</div>
+        <div className="button-area">
+          <button className="plus" onClick={this.onPlusClick}>+</button>
+          <button className="minus" onClick={this.onMinusClick}>-</button>
+          <button className="reset" onClick={this.onResetClick}>reset</button>
+        </div>
+        </div>
       </div>
-    );
+      );
+  }
+  onPlusClick = () =>{
+    this.setState({number:this.state.number + 1})
+  }
+  onMinusClick = () =>{
+    this.setState({number:this.state.number - 1})
+  }
+  onResetClick = () =>{
+    this.setState({number:0})
   }
 }
 
-export default App;
+
+export default Count;
--- src/index.js
+++ src/index.js
@@ -1,40 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import Count from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-class Count extends React.Component{
-	constructor(props) {
-	  super(props);	
-	  this.state = {number: 0};
-	}
-
-	render(){
-		return(
-			<div>
-				<h1>カウンター</h1>
-				<div className="wrap">
-				<div className="num">{this.state.number}</div>
-				<div className="button-area">
-					<button className="plus" onClick={this.onPlusClick}>+</button>
-					<button className="minus" onClick={this.onMinusClick}>-</button>
-					<button className="reset" onClick={this.onResetClick}>reset</button>
-				</div>
-				</div>
-			</div>
-			);
-	}
-	onPlusClick = () =>{
-		this.setState({number:this.state.number + 1})
-	}
-	onMinusClick = () =>{
-		this.setState({number:this.state.number - 1})
-	}
-	onResetClick = () =>{
-		this.setState({number:0})
-	}
-}
 
 ReactDOM.render(<Count />, document.getElementById("root"));
 
