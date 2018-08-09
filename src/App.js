e4a11b546dc500f0fcf63dc3ab0778e0202f65ef
Tahara <tahara@studiom-web.net>
2018-08-09 15:02:37 +0900

    Axiosを使う

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {member:[]};
+  }
+
+  memberList(list){
+    const memberList = list.map((member,index) =>{
+      return (
+        <li>
+          {member.name} {member.age}
+        </li>
+        )
+    })
+    return <ul>{memberList}</ul>
+  }
   render() {
     return (
       <div className="App">
@@ -13,9 +30,18 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.getJson}>click</button>
+        {this.memberList(this.state.member)}
       </div>
     );
   }
+  getJson = () => {
+    const url = "https://api.myjson.com/bins/rjq9g";
+
+    axios.get(url).then(res => {
+      this.setState(res.data);
+  })
+  };
 }
 
 export default App;
