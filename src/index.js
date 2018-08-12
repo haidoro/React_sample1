c59823e64bf099c1ed5d43fd1ff5240c30058863
Tahara <tahara@studiom-web.net>
2018-08-13 07:12:20 +0900

    ファイルの整理

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import registerServiceWorker from './registerServiceWorker';
 
 const ReturnReactElement = (props) => {
   return (<div>
-	  		<h2>{props.name}</h2>
-	  		<p>{props.age}</p>
-  		</div>)
+	  <h2>{props.name}</h2>
+	  <p>{props.age}</p>
+  </div>)
 }
 
 ReactDOM.render(<ReturnReactElement name="Tahara" age="38"/>, document.getElementById("root"));
